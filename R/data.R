9e3024e8a6d54d2b85bde090acc7f58a900075e4
Bineyam Tafesse <bineyam.tafesse@hotmail.com>
2020-11-24 16:47:24 -0500

    Adding the data!

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Edgar Anderson's Iris Data
+#'
+#' This famous (Fisher's or Anderson's) iris data set gives the measurements in centimeters of the variables 
+#' sepal length and width and petal length and width, respectively, for 50 flowers from each of 3 species of iris.
+#'  The species are Iris setosa, versicolor, and virginica.
+#'
+#' @format A data frame with 150 rows and 5 variables:
+#' \describe{
+#'   \item{ID}{this id}
+#'   \item{X}{this x}
+#'   ...
+#' }
+#' @source iris datasets from base R
+#' @source Fisher, R. A. (1936) The use of multiple measurements in taxonomic problems. Annals of Eugenics, 7, Part II, 179–188.
+"myData"
